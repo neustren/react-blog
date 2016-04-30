9ca868c4a712757dce8144fcc3da7dc42588ef3d
William Franco <william@effy.com.br>
2016-04-30 14:30:21 -0300

    w

--- app/containers/inicial/index.js
+++ app/containers/inicial/index.js
@@ -76,23 +76,23 @@ export default class Inicial extends Component {
         </div>
         <div className="container">
           <div className={styles.linhaFlex}>
-          <div className={styles.boxTags} style={{backgroundColor: '#67b35a', borderTop: '4px solid #67b35a'}}>
+          <div className={styles.boxTags} style={{backgroundColor: '#67b35a', borderTop: '0.2em solid #67b35a'}}>
             <img className={styles.iconeFiltro} src={iconeFiltroRecentes}></img>
             <div className={styles.nomeFiltro}  style={{color: '#555555'}}>RECENTES</div>
           </div>
-          <div className={styles.boxTags} style={{backgroundColor: '#ffef00', borderTop: '4px solid #ffef00'}}>
+          <div className={styles.boxTags} style={{backgroundColor: '#ffef00', borderTop: '0.2em solid #ffef00'}}>
             <img className={styles.iconeFiltro} src={iconeFiltroFinancas}></img>
             <div className={styles.nomeFiltro}  style={{color: '#555555'}}>FINANÇAS</div>
           </div>
-          <div className={styles.boxTags} style={{backgroundColor: '#005aa1', borderTop: '4px solid #005aa1'}}>
+          <div className={styles.boxTags} style={{backgroundColor: '#005aa1', borderTop: '0.2em solid #005aa1'}}>
             <img className={styles.iconeFiltro} src={iconeFiltroMundo}></img>
             <div className={styles.nomeFiltro} style={{color: '#001d58'}}>MUNDO</div>
           </div>
-          <div className={styles.boxTags} style={{backgroundColor: '#a8daf4', borderTop: '4px solid #a8daf4'}}>
+          <div className={styles.boxTags} style={{backgroundColor: '#a8daf4', borderTop: '0.2em solid #a8daf4'}}>
             <img className={styles.iconeFiltro} src={iconeFiltroImoveis}></img>
             <div className={styles.nomeFiltro} style={{color: '#6395af'}}>IMOVEIS</div>
           </div>
-          <div className={styles.boxTags} style={{backgroundColor: '#333333', borderTop: '4px solid #333333'}}>
+          <div className={styles.boxTags} style={{backgroundColor: '#333333', borderTop: '0.2em solid #333333'}}>
             <img className={styles.iconeFiltro} src={iconeFiltroEstilo}></img>
             <div className={styles.nomeFiltro} style={{color: '#595959'}}>ESTILO</div>
           </div>
