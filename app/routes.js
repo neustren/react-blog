4cd0b7438b9574291c43366517e0a94e3ad2d696
William Franco <William Franco>
2016-07-24 16:16:25 -0300

    w

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,14 @@
+// *************************************************************************
+// * Copyright (C) 2016 Eletrônica Farias Franco LTDA - Todos os direitos reservados.
+// *
+// * This file is part of Brasilcap Blog Project.
+// * The complete set of authors may be found at http://www.effy.com.br
+// * Unauthorized copying of this file, via any medium is strictly prohibited
+// * This code can not be copied and/or distributed without the express
+// * permission of EFFY
+// * Effy - support@effy.com.br
+// *************************************************************************
+
 import React from 'react';
 import { Router, Route, IndexRoute } from 'react-router'
 
