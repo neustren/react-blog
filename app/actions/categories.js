6cd71674ae5b12e10fe8d8c1e425bd094ee140e2
William Franco <William Franco>
2016-07-24 16:23:55 -0300

    License

--- app/actions/categories.js
+++ app/actions/categories.js
@@ -1,13 +1,14 @@
-// *************************************************************************
-// * Copyright (C) 2016 Eletrônica Farias Franco LTDA - Todos os direitos reservados.
-// *
-// * This file is part of Brasilcap Blog Project.
-// * The complete set of authors may be found at http://www.effy.com.br
-// * Unauthorized copying of this file, via any medium is strictly prohibited
-// * This code can not be copied and/or distributed without the express
-// * permission of EFFY
-// * Effy - support@effy.com.br
-// *************************************************************************
+/**
+* @license
+* Copyright (C) 2016 Eletrônica Farias Franco LTDA - Todos os direitos reservados.
+*
+* This file is part of Brasilcap Blog Project.
+* The complete set of authors may be found at http://www.effy.com.br
+* Unauthorized copying of this file, via any medium is strictly prohibited
+* This code can not be copied and/or distributed without the express
+* permission of EFFY
+* Effy - support@effy.com.br
+*************************************************************************/
 
 import client from 'superagent';
 export const CATEGORIES = 'CATEGORIES';
--- app/actions/index.js
+++ app/actions/index.js
@@ -1,13 +1,14 @@
-// *************************************************************************
-// * Copyright (C) 2016 Eletrônica Farias Franco LTDA - Todos os direitos reservados.
-// *
-// * This file is part of Brasilcap Blog Project.
-// * The complete set of authors may be found at http://www.effy.com.br
-// * Unauthorized copying of this file, via any medium is strictly prohibited
-// * This code can not be copied and/or distributed without the express
-// * permission of EFFY
-// * Effy - support@effy.com.br
-// *************************************************************************
+/**
+* @license
+* Copyright (C) 2016 Eletrônica Farias Franco LTDA - Todos os direitos reservados.
+*
+* This file is part of Brasilcap Blog Project.
+* The complete set of authors may be found at http://www.effy.com.br
+* Unauthorized copying of this file, via any medium is strictly prohibited
+* This code can not be copied and/or distributed without the express
+* permission of EFFY
+* Effy - support@effy.com.br
+*************************************************************************/
 
 export const ROOT_URL = 'http://tymo.com.br/bblog';
 export const SITE_URL = 'http://localhost:3100';
